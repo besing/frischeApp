1486d22a7e45f18d7830ec2b17a76241388e280d
Benni MacBookAir <benni.singer@gmail.com>
2016-09-24 20:30:04 +0200

    Get DIGEST-AUTH working finally (kind of) - on different Server (switched to own Test setup w/o additional HTTP Basic Auth (like Frischepost Test Env)) - got it running with help of NPM digestAuthRequest and CryptoJS (and base-64, kind of.. even though not imported) - needed to add imports/exports in original NPMs --> not good - probably also doable only with native JS-Fetch, didn't manage w/ all the Digest-Roundtrip (Req-Res-Req-Res) - "npmModified_" files are only for backup purposes, not included in any kind - apiAuth.php is default PHP Client-API from Shopware, just for API testing purposes - add XCode HTTP Calls (insecure..) (for now)

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -12,12 +6,83 @@ import {
   View
 } from 'react-native';
 
+import digestAuthRequest from 'digest-auth-request/digestAuthRequest';
+
+function digest() {
+  var url = 'http://frische-app.de.shopware-hosting.com/api/articles/1';
+  var getRequest = new digestAuthRequest('GET', url, 'benni', 'Th2m3XUzQKXIKgfGzT5EhN3sHmV5UVr0uOkyitAZ');
+
+  getRequest.request(function (data) {
+    console.log('Data: ', data);
+  }, function (error) {
+    console.error('Error: ', error);
+  })
+}
+digest();
+
+/*
+function apiAuth() {
+  var params = {
+    method: 'GET',
+    headers: {
+      'Accept': '*!/!*',
+      'Content-Type': 'application/json',
+      'Authorization': 'Digest',
+      username: 'benni',
+      realm: 'Shopware REST-API',
+      nonce: 'b60920f74be06df116aa13847b841697',
+      uri: '/api/articles/1',
+      response: '84e6d53b03122b4407b9a025bf214416',
+      opaque: 'd75db7b160fe72d1346d2bd1f67bfd10',
+      algorithm: 'MD5',
+      qop: 'auth',
+      nc: '00000001',
+      cnonce: '6801d4e79d66f979'
+    },
+  };
+
+  fetch('http://frische-app.de.shopware-hosting.com/api/articles/1', params)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log (responseJson);
+    })
+    .catch((error) => {
+      console.error('Fetch fehlgeschlagen: ', error);
+    })
+}
+// apiAuth();
+*/
+
+/*
+function xhr() {
+  // https://forum.shopware.com/discussion/36056/rest-api-verbindung-pure-js
+  var client = new XMLHttpRequest();
+  client.open(
+    'GET',
+    'http://frische-app.de.shopware-hosting.com/api/articles',
+    true,
+    'benni',
+    'Th2m3XUzQKXIKgfGzT5EhN3sHmV5UVr0uOkyitAZ'
+  );
+  client.setRequestHeader(
+    'Content-Type: application/json; charset=utf-8'
+  );
+  client.onreadystatechange = function() {
+    alert(this.responseText);
+    console.log(this.responseText);
+  };
+  client.send();
+}
+// xhr();
+*/
+
+
 class frischeApp extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          1234
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
