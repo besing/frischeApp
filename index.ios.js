a7c7c485dfc9e8f92f8f9e66e6d13a4e8cde317d
Benni MacBookAir <benni.singer@gmail.com>
2016-09-25 13:49:24 +0200

    Get HTTP-Digest-Auth working *really* now (not cached yet though)  - fork npm digestAuthRequest with own modifications for RN-compatability (+create GH Pull Requets)  - npm pack as *.tar.gz for later installs (from local)  - npm base-64 was obsolete

--- index.ios.js
+++ index.ios.js
@@ -6,75 +6,23 @@ import {
   View
 } from 'react-native';
 
-import digestAuthRequest from 'digest-auth-request/digestAuthRequest';
+import digestAuthRequestRn from 'digest-auth-request-rn';
 
+
+// Auth via NPM DigestAuthRequest (forked)
 function digest() {
-  var url = 'http://frische-app.de.shopware-hosting.com/api/articles/1';
-  var getRequest = new digestAuthRequest('GET', url, 'benni', 'Th2m3XUzQKXIKgfGzT5EhN3sHmV5UVr0uOkyitAZ');
+  var url = 'http://frische-app.de.shopware-hosting.com/api/articles';
+  var apiUser = 'benni';
+  var apiKey = 'Th2m3XUzQKXIKgfGzT5EhN3sHmV5UVr0uOkyitAZ';
+  var getRequest = new digestAuthRequestRn('GET', url, apiUser, apiKey);
 
   getRequest.request(function (data) {
-    console.log('Data: ', data);
+    console.log('Auth Data: ', data);
   }, function (error) {
     console.error('Error: ', error);
   })
 }
 digest();
-
-/*
-function apiAuth() {
-  var params = {
-    method: 'GET',
-    headers: {
-      'Accept': '*!/!*',
-      'Content-Type': 'application/json',
-      'Authorization': 'Digest',
-      username: 'benni',
-      realm: 'Shopware REST-API',
-      nonce: 'b60920f74be06df116aa13847b841697',
-      uri: '/api/articles/1',
-      response: '84e6d53b03122b4407b9a025bf214416',
-      opaque: 'd75db7b160fe72d1346d2bd1f67bfd10',
-      algorithm: 'MD5',
-      qop: 'auth',
-      nc: '00000001',
-      cnonce: '6801d4e79d66f979'
-    },
-  };
-
-  fetch('http://frische-app.de.shopware-hosting.com/api/articles/1', params)
-    .then((response) => response.json())
-    .then((responseJson) => {
-      console.log (responseJson);
-    })
-    .catch((error) => {
-      console.error('Fetch fehlgeschlagen: ', error);
-    })
-}
-// apiAuth();
-*/
-
-/*
-function xhr() {
-  // https://forum.shopware.com/discussion/36056/rest-api-verbindung-pure-js
-  var client = new XMLHttpRequest();
-  client.open(
-    'GET',
-    'http://frische-app.de.shopware-hosting.com/api/articles',
-    true,
-    'benni',
-    'Th2m3XUzQKXIKgfGzT5EhN3sHmV5UVr0uOkyitAZ'
-  );
-  client.setRequestHeader(
-    'Content-Type: application/json; charset=utf-8'
-  );
-  client.onreadystatechange = function() {
-    alert(this.responseText);
-    console.log(this.responseText);
-  };
-  client.send();
-}
-// xhr();
-*/
 
 
 class frischeApp extends Component {
