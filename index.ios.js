535742493347d1c135e0b61df1f5be7c5e1d2d69
Benni MacBookAir <benni.singer@gmail.com>
2016-09-27 16:40:52 +0200

    Build (successful) POST-Request through npm package  - try subsequent Calls through XmlHttpRequest (XHR), not getting authorized (401), even though sending Auth Req Header  - also tried Fetch, same problem  - (bigger) problem with "Writing" Calls (PUT/POST) is that the endpoint needs to be exact (not a big object like possible in GET) -- even with same endpoint 401  - add currentTime Vars for debugging  - copy/paste PHP Api-Client from Shopware Docs as a reference

--- index.ios.js
+++ index.ios.js
@@ -9,47 +9,140 @@ import {
 import digestAuthRequestRn from 'digest-auth-request-rn';
 import { digestAuthHeader } from 'digest-auth-request-rn';
 
+const apiCredentials_test = {
+  url: 'http://138.68.72.48/api/articles',
+  apiUser: 'Benni',
+  apiKey: 'Z1ZpvndKil5qq1xImrUZ0EAvTFehSFaQndTP6SVU'
+};
 
-// Auth via NPM DigestAuthRequest (forked)
-function digest() {
-  var url = 'http://frische-app.de.shopware-hosting.com/api/articles';
-  var apiUser = 'benni';
-  var apiKey = 'Th2m3XUzQKXIKgfGzT5EhN3sHmV5UVr0uOkyitAZ';
+const apiCredentials_dev = { // LATER?: Auth on /api, not on /api/... (e.g. /articles)
+  url: 'http://frische-app.de.shopware-hosting.com/api/articles',
+  apiUser: 'benni',
+  apiKey: 'Th2m3XUzQKXIKgfGzT5EhN3sHmV5UVr0uOkyitAZ'
+};
+
+// Current Time Logging for info
+const currentHrs = new Date().getHours();
+const currentMins = new Date().getMinutes();
+const currentTime = currentHrs + ':' + currentMins + ' Uhr';
+console.debug('Time of Response: ' + currentTime);
+
+/*
+function digestGet() {
+  var url = apiCredentials_dev.url;
+  var apiUser = apiCredentials_dev.apiUser;
+  var apiKey = apiCredentials_dev.apiKey;
   var getRequest = new digestAuthRequestRn('GET', url, apiUser, apiKey);
 
   getRequest.request(function (data) {
     console.log('Auth Data: ', data);
 
-    // TODO: Zweiten (+ alle folgenden Calls) nicht so verschachteln, lieber s.u. TODO
+    function getCall() {
+      fetch(apiCredentials_dev.url, { method: 'GET', headers: {'Authorization': digestAuthHeader} })
+     .then((response) => response.json())
+     .then((responseData) => {
+       console.log('GET Product #3 (name): ', responseData.data[2].name);
+     })
+    }
+    // getCall();
 
-    function basicCall() {
-      fetch('http://frische-app.de.shopware-hosting.com/api/articles', {
-
-        // TODO: leider (vorerst) gleiche URL wie bei Auth (komplettes API-Array), sonst 401er --> evtl. zu beheben, aber evtl. auch Auth bloß für die eine exakte URL --> dann (evtl. performance-schwierige) Calls auf komplettes Objekt immer? Oder egal?
-
-        method: 'GET',
-        headers: {
-          'Authorization': digestAuthHeader,
-        }
+    function putCall() {
+      fetch(apiCredentials_dev.url, {
+        method: 'PUT',
+        headers: {'Authorization': digestAuthHeader, 'Content-type': 'application/json'},
+        body: JSON.stringify({
+          name: 'Carbon D: Api-PUT-Updated!'
         })
-       .then((response) => response.json())
-       .then((responseData) => {
-         console.log('Basic Call Data: ', responseData);
-       })
-       .done(); // TODO: necessary?
-       }
-    basicCall();
+      })
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log('PUT Response: ', responseData);
+      })
+    }
+    // putCall();
 
+    function postCall() {
+      fetch('http://frische-app.de.shopware-hosting.com/api/categories', {
+        method: 'POST',
+        headers: {'Authorization': digestAuthHeader, 'Content-type': 'application/json'},
+        body: JSON.stringify({
+          name: 'Kategorie via API #2',
+          parentId: 5
+        })
+      })
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log('POST Response: ', responseData);
+      })
+    }
+    postCall();
 
   }, function (error) {
     console.error('Error: ', error);
   })
 }
-digest();
+// digestGet();
+*/
+
+function digestPost() {
+  var postData = {
+    name: 'Kat. via API (' + currentTime + ')',
+    parentId: 5
+  };
+
+  var postReq = new digestAuthRequestRn('POST',
+    'http://frische-app.de.shopware-hosting.com/api/categories', apiCredentials_dev.apiUser,
+    apiCredentials_dev.apiKey);
+
+  postReq.request(function (data) {
+    console.log('digestPost SUCCESS: ', data);
+
+/*
+    function getCallFromPostAuth() {
+      fetch('http://frische-app.de.shopware-hosting.com/api/categories', { method: 'GET', headers: {'Authorization': digestAuthHeader} })
+        .then((response) => response.json())
+        .then((responseData) => {
+          console.log('GET Result: ', responseData);
+        })
+    }
+    // getCallFromPostAuth();
+*/
+
+    // trying the subsequent API Call through XHR (instead of Fetch)
+    function xhrGet(url, success, error) {
+      var req = new XMLHttpRequest();
+      req.open('GET', url, true);
+      req.setRequestHeader('Authorization', digestAuthHeader);
+      req.onreadystatechange = function () {
+        var status = null;
+        var data = null;
+        if (req.readyState == 4) {
+          status = req.status;
+          if (status == 200) {
+            data = JSON.parse(req.responseText);
+            success && success(data);
+          } else {
+            error && error(status);
+          }
+        }
+      };
+      req.send();
+    }
+    xhrGet('http://frische-app.de.shopware-hosting.com/api/categories', function (data) {
+      console.debug('XHR SUCCESS: ', data);
+    }, function (status) {
+      console.error('XHR ERROR: ', status);
+    });
+
+
+  }, function (errorCode) {
+    console.error('digestPost ERROR: ', errorCode)
+  }, postData);
+}
+digestPost();
 
 
 // TODO: Erst andere Calls ausführen, wenn authentifiziert! (React Lifecycle Methods?)
-
 
 
 
