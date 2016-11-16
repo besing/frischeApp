7614183687cbdc0c50eb032745183ca9b98a78b5
Benni MacBook <benni.singer@gmail.com>
2016-11-16 12:32:19 +0100

    Embed (most basic) Camera Component with default properties/styles - (!!) important step: add plist Values to Xcode for CameraUsage + MicrophoneUsage

--- app/scenes/CustomerScan.js
+++ app/scenes/CustomerScan.js
@@ -2,14 +2,50 @@
 
 import React, { Component } from 'react';
 import {
-  View,
+  Dimensions,
+  StyleSheet,
   Text,
+  View
 } from 'react-native';
+
+import Camera from 'react-native-camera';
+
+import globalStyles from '../config/globalStyles';
+
 
 export default class CustomerScan extends Component {
   render() {
     return (
-      <View />
+      <View style={globalStyles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}
+          captureAudio={false}
+        >
+          <Text>Hier Cam</Text>
+        </Camera>
+      </View>
     )
   }
 }
+
+const styles = { // TODO: no Stylesheet.create necessary?
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+};
