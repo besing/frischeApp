ea50245efbc3351cf30f233695b8f5ccbad209db
Benni MacBook <benni.singer@gmail.com>
2016-11-16 15:55:17 +0100

    Create processQrCode Method (which works on more basic QR code than my test code) - plus: first rough Styles on Cam Viewfinder - plus: Xcode iOS Requirement Edit 8.0 -> 8.2 after Warning

--- app/scenes/CustomerScan.js
+++ app/scenes/CustomerScan.js
@@ -17,18 +17,20 @@ export default class CustomerScan extends Component {
     super(props);
 
     this._takePicture = this._takePicture.bind(this);
+    this._processQrCode = this._processQrCode.bind(this);
   }
 
   render() {
     return (
-      <View style={globalStyles.container}>
+      <View style={[globalStyles.container, {justifyContent: 'center', alignItems: 'center'}]}>
         <Camera
           ref={(cam) => {
             this.camera = cam;
           }}
-          style={styles.preview}
-          aspect={Camera.constants.Aspect.fill}
+          style={[styles.preview, {borderColor: 'yellow', borderWidth: 1, width: width*0.8, height: width*0.8}]}
+          aspect="fill"
           captureAudio={false}
+          onBarCodeRead={this._processQrCode}
         >
           <Text style={styles.capture} onPress={this._takePicture}>[CAPTURE]</Text>
         </Camera>
@@ -41,15 +43,17 @@ export default class CustomerScan extends Component {
       .then((data) => console.log(data))
       .catch(err => console.error(err));
   }
+
+  _processQrCode(event) {
+    console.info('QR Code erkannt.');
+    console.log(event.data);
+  }
 }
 
 const styles = StyleSheet.create({ // TODO: no Stylesheet.create necessary?
   preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
+    justifyContent: 'center',
     alignItems: 'center',
-    width: width,
-    height: height,
   },
   capture: {
     flex: 0,
