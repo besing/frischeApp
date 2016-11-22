e647b790a3ca428f342bafd6eb0027293fd2262a
Benni MacBook <benni.singer@gmail.com>
2016-11-22 12:40:24 +0100

    Add new UI elements for ArticlesList: Button "Submit" + "Back to edit" - only render if ConfirmButton was pressed = filtered View is active

--- app/scenes/ArticlesList.js
+++ app/scenes/ArticlesList.js
@@ -21,24 +21,56 @@ export default class ArticlesList extends Component {
 
     this.state = {
       confirmButtonClicked: false,
-      articleListFetched: false
+      articleListFetched: false,
+      filteredViewActive: false
     };
 
-    this._hideConfirmButton = this._hideConfirmButton.bind(this);
+    this._articlesFetched = this._articlesFetched.bind(this);
+    this._hideConfirmButtonOnFilteredView = this._hideConfirmButtonOnFilteredView.bind(this);
   }
 
   render() {
     const confirmButton = (
-      // render Button-JSX only when articleListFetched (otherwise show empty View)
-        this.state.articleListFetched && <IconMaterial.Button
+      // render Button-JSX only when articleListFetched + not yet in filtered View (otherwise empty View)
+        this.state.articleListFetched && !this.state.filteredViewActive && <IconMaterial.Button
         name="playlist-add-check"
         backgroundColor="sandybrown"
-        onPress={() => this.setState({confirmButtonClicked: true})}
+        onPress={() => {
+          this.setState({confirmButtonClicked: true});
+          this._hideConfirmButtonOnFilteredView();
+        }}
+        underlayColor="#000"
+        size={25}
+        borderRadius={0}
+        disabled={false}
+      >
+        Eingabe überprüfen
+      </IconMaterial.Button>
+    ) || <View/>;
+
+    const backToEditButton = (
+        this.state.filteredViewActive && <IconMaterial.Button
+        name="edit"
+        backgroundColor="red"
+        onPress={null}
         underlayColor="#000"
         size={25}
         borderRadius={0}
       >
-        Eingabe überprüfen
+        Zurück zur Eingabe
+      </IconMaterial.Button>
+    ) || <View/>;
+
+    const submitButton = (
+        this.state.filteredViewActive && <IconMaterial.Button
+        name="send"
+        backgroundColor="green"
+        onPress={null}
+        underlayColor="#000"
+        size={25}
+        borderRadius={0}
+      >
+        Alles ok, Daten hochladen
       </IconMaterial.Button>
     ) || <View/>;
 
@@ -46,13 +78,19 @@ export default class ArticlesList extends Component {
       <View style={[globalStyles.container, {backgroundColor: '#eee', paddingTop: 64}]}>
         <NavbarSubtitle>{this.props.email}</NavbarSubtitle>
         <GetDepositArticles filterListOnButtonConfirm={this.state.confirmButtonClicked}
-                            hideConfirmButtonWhileLoading={this._hideConfirmButton} />
+                            hideConfirmButtonWhileLoading={this._articlesFetched} />
         {confirmButton}
+        {backToEditButton}
+        {submitButton}
       </View>
     );
   }
 
-  _hideConfirmButton() {
+  _articlesFetched() {
     this.setState({articleListFetched: true});
+  }
+
+  _hideConfirmButtonOnFilteredView() {
+    this.setState({filteredViewActive: true})
   }
 }
