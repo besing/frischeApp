998f30464a9a8b74e6fa208d17d6a14af7cf8f28
Benni MacBook <benni.singer@gmail.com>
2017-02-19 20:00:19 +0100

    Switch to Placeholder Config File for individual API credentials on repo

---
+++ app/config/settings_placeholder.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const appConfig = {
+  apiCredentials_test: { // switch to Production config later
+    url: 'INSERT SHOPWARE GENERAL API ENDPOINT http://.../api',
+    apiUser: 'INSERT API USER',
+    apiKey: 'INSERT API KEY'
+  }
+};
+
+export default appConfig;
